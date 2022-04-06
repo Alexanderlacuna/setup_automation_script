84658a2d8fedcdb05c933160a12b7529be71ece0
Alexander <alexanderkabua@gmail.com>
2022-04-07 01:23:11 +0300

    code to unzip tar 7z file

--- genetwork_setup.sh
+++ genetwork_setup.sh
@@ -23,7 +23,14 @@ wget "http://ipfs.genenetwork.org/ipfs/QmXQy3DAUWJuYxubLHLkPMNCEVq1oV7844xWG2d1G
 
 7z x genotype_files.tar.7z
 
+7z x -so genotype_files.tar.7z | tar xf - -C ./
 
 
+
+RED='\033[0;31m'
+
+echo -e "${RED}Mock installation"
 
 env GUIX_PACKAGE_PATH="/home/kabui/guix-bioinformatics/:/home/kabui/guix-past/modules" GENENETWORK_FILES=/home/kabui/data/genotype_files  ~/opt/guix-pull/bin/guix package -p ~/opt/python3-genenetwork2 --substitute-urls="http://guix.genenetwork.org  https://ci.guix.gnu.org   https://mirror.hydra.gnu.org" -i genenetwork2 --fallback --dry-run
+
+
