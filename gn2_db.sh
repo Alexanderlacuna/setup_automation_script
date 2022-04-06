b35fdf2ff890a3b1d6dcf215be59bdbc4b21237c
Alexander <alexanderkabua@gmail.com>
2022-04-07 02:36:05 +0300

    minor fix for gn2 db file

--- gn2_db.sh
+++ gn2_db.sh
@@ -5,8 +5,10 @@
 
 cd ~/Downloads
 
-gzip -dk file.gz
+gzip -dk db.2020-10-21.143234.sql.gz
+echo -e "${RED} import the sql file"
 
-mysql -u kabui -p database_name < ./file_here
+
+mysql -u kabui -p db_webqtl < db.2020-10-21.143234.sql
 
 
